2bd5b06e618098d276f28d205d055af15bf855c4
Tane <jonathanlopeti@gmail.com>
2026-05-07 13:47:15 +1000

    add portfolio features

---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+export function Header() {
+    return (
+        <header className="flex items-center justify-between border-b h-10">
+            <div>
+            </div>
+        </header>
+    )
+}
---
+++ src/features/About.tsx
@@ -0,0 +1,16 @@
+import { UserRound } from "lucide-react";
+import { Section } from "../components/Section";
+
+export function About() {
+    return (
+        <Section id="about">
+            <div className="space-y-4">
+                <div className="flex gap-2 items-center">
+                    <UserRound className="size-5" />
+                    <h3 className="text-xl">About</h3>
+                </div>
+                <p>Over the past 7+ years, I have delivered scalable, user-centric software across banking and public-sector domains. I specialize in React and Spring Boot with a strong focus on clean architecture, simplicity, performance, and both user and developer experience.</p>
+            </div>
+        </Section>
+    )
+}
