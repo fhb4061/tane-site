38ff0da89274d1d15055adcc3d9035fbfb125297
Tane <jonathanlopeti@gmail.com>
2026-06-03 22:06:06 +1000

    dark mode toggle

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,10 @@
+import { ModeToggle } from "./ModeToggle";
+
 export function Header() {
     return (
         <header className="flex backdrop-blur sticky top-0 items-center justify-between h-14 border-b border-primary">
             <div className="p-3 flex justify-between items-center w-full">
+                <ModeToggle />
             </div>
         </header>
     )
